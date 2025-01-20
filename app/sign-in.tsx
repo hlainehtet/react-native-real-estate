4d144bf0edbb1a808e7ebfd69dc30884c4f89076
Hlaing Htet <99160494+Hlaing-Htet@users.noreply.github.com>
2025-01-20 21:22:32 +0700

    assets and font setup

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,48 @@
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SingIn = () => {
+  const handleLogin = () => {};
   return (
-    <View>
-      <Text>SingIn</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white ">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10 ">
+          <Text className="text-base text-center text-blue-200 uppercase font-rubik">
+            Welcome to ReState
+          </Text>
+          <Text className="mt-2 text-3xl text-center font-rubik-bold text-black-300">
+            Let's Get You Closer to {"\n"}
+            <Text className=" text-primary-300">Your Ideo Home</Text>
+          </Text>
+          <Text className="mt-12 text-lg text-center font-rubik text-black-200">
+            Login to Restate with Google
+          </Text>
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="w-full py-4 mt-5 bg-white rounded-full shadow-md shadow-zinc-300 "
+          >
+            <View className="flex flex-row items-center justify-center gap-2">
+              <Image
+                source={icons.google}
+                className="w-5 h-5 "
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
